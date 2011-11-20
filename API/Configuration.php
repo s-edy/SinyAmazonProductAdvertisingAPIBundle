d9597e3c3e5a35eb210608ff53f5158079d740f6
edy <sinycourage@gmail.com>
2011-11-21 08:18:36 +0900

    added an abstract Configuration class.

---
+++ API/Configuration.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * This file is a part of SinyAmazonProductAdvertisingAPIBundle package.
+ *
+ * @author Shinichiro Yuki <sinycourage@gmail.com>
+ */
+
+namespace Siny\Amazon\ProductAdvertisingAPIBundle\API;
+
+/**
+ * This is a abstract class that configure the Amazon API
+ *
+ * @package SinyAmazonProductAdvertisingAPI
+ * @subpackage API
+ * @author Shinichiro Yuki <sinycourage@gmail.com>
+ */
+abstract class Configuration
+{
+    /**
+     * Configuration parameters
+     * @var array
+     */
+    private $parameters = array();
+
+    /**
+     * Get all parameters
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * Clear all parameters
+     *
+     * @return Siny\Amazon\ProductAdvertisingAPIBundle\API\Configuration
+     */
+    public function clear()
+    {
+        $this->parameters = array();
+
+        return $this;
+    }
+
+    /**
+     * Set a parameter key-value pair
+     *
+     * @param string $key  - Parameter key
+     * @param mixed $value - Value
+     * @return Siny\Amazon\ProductAdvertisingAPIBundle\API\Configuration
+     */
+    protected function set($key, $value)
+    {
+        $this->parameters[$key] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get a parameter from key
+     *
+     * @param string $key
+     * @return mixed
+     * @throws InvalidArgumentException - If the parameter of the specified key does not exist
+     */
+    protected function get($key)
+    {
+        if ($this->has($key) === false) {
+            throw new \InvalidArgumentException(sprintf("The value of speficied key index was not found. key=[%s]", $key));
+        }
+        return $this->parameters[$key];
+    }
+
+    /**
+     * Has a parameter value of key
+     *
+     * @param string $key
+     * @return boolean - Whether the parameter of the specified key is exist
+     */
+    protected function has($key)
+    {
+        return isset($this->parameters[$key]);
+    }
+}
---
+++ Tests/API/ConfigurationTest.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * This test file is a part of SinyAmazonProductAdvertisingAPIBundle package.
+ *
+ * @author Shinichiro Yuki <sinycourage@gmail.com>
+ */
+
+namespace Siny\Amazon\ProductAdvertisingAPIBundle\Tests\API;
+
+use Siny\Amazon\ProductAdvertisingAPIBundle\API\Configuration;
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    const DUMMY_KEY   = 'dummyKey';
+    const DUMMY_VALUE = 'dummyValue';
+    const DUMMY_AWS_ACCESS_KEY_ID = 'dummy_aws_access_key_id';
+    const DUMMY_SECRET_ACCESS_KEY = 'dummy_secret_access_key';
+    const DUMMY_ASSOCIATE_TAG     = 'dummy_associate_tag';
+
+    private $configuration;
+    private $dateTime;
+
+    public function setUp()
+    {
+        $this->configuration = $this->getMockForAbstractClass('Siny\Amazon\ProductAdvertisingAPIBundle\API\Configuration');
+    }
+
+    /**
+     * Returns array type when invoking toArray()
+     */
+    public function testReturnsArrayTypeWhenInvokingToArray()
+    {
+        $this->assertType('array', $this->configuration->toArray(), "The returns wasn't array.");
+    }
+
+
+    /**
+     * Returns empty array when invoking toArray() in the case of default
+     */
+    public function testReturnsEmptyArrayWhenInvokingToArrayInTheCaseOfDefault()
+    {
+        $this->assertEmpty($this->configuration->toArray(), "There weren't parameters.");
+    }
+
+    /**
+     * A configuration object will be returned after you set something
+     */
+    public function testSelfObjectWillBeReturnedWhenInvokingSet()
+    {
+        $method = new \ReflectionMethod('Siny\Amazon\ProductAdvertisingAPIBundle\API\Configuration', 'set');
+        $method->setAccessible(true);
+        $this->assertInstanceOf(
+        	'Siny\Amazon\ProductAdvertisingAPIBundle\API\Configuration',
+        	$method->invoke($this->configuration, self::DUMMY_KEY, self::DUMMY_VALUE),
+        	"A self object wasn't returned when invoking set().");
+        return $this->configuration;
+    }
+
+    /**
+     * Get parameter
+     *
+     * @depends testSelfObjectWillBeReturnedWhenInvokingSet
+     * @param Configuration $configuration
+     */
+    public function testGet(Configuration $configuration)
+    {
+        $method = new \ReflectionMethod('Siny\Amazon\ProductAdvertisingAPIBundle\API\Configuration', 'get');
+        $method->setAccessible(true);
+        $this->assertSame(
+            self::DUMMY_VALUE, $method->invoke($configuration, self::DUMMY_KEY),
+        	"Get value wasn't same.");
+    }
+
+    /**
+     * Returns array when invoking toArray()
+     *
+     * @depends testSelfObjectWillBeReturnedWhenInvokingSet
+     * @param Configuration $configuration
+     */
+    public function testReturnArrayWhenInvokingToArray(Configuration $configuration)
+    {
+        $this->assertSame(
+            array(self::DUMMY_KEY => self::DUMMY_VALUE),
+            $configuration->toArray(),
+        	"Parameters which was set wasn't returned.");
+    }
+
+    /**
+     * An exception will occur if trying to get when specifying a wrong key
+     *
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage The value of speficied key index was not found. key=[dummyKey]
+     */
+    public function testAnExceptionWillOccurIfTryingToGetWhenSpecifyingAWrongKey()
+    {
+        $method = new \ReflectionMethod('Siny\Amazon\ProductAdvertisingAPIBundle\API\Configuration', 'get');
+        $method->setAccessible(true);
+        $method->invoke($this->configuration, self::DUMMY_KEY);
+    }
+
+    /**
+     * Has the parameter which is specified key
+     *
+     * @depends testSelfObjectWillBeReturnedWhenInvokingSet
+     */
+    public function testHasTheParameterWhichIsSpecifiedKey(Configuration $configuration)
+    {
+        $method = new \ReflectionMethod('Siny\Amazon\ProductAdvertisingAPIBundle\API\Configuration', 'has');
+        $method->setAccessible(true);
+        $this->assertTrue($method->invoke($configuration, self::DUMMY_KEY), "Has not parameter.");
+    }
+
+    /**
+     * Clear all parameters
+     *
+     * @depends testGet
+     * @param Configuration $configuration
+     */
+    public function testSelfObjectWillBeReturnedWhenInvokingClear()
+    {
+        $this->assertInstanceOf(
+    		'Siny\Amazon\ProductAdvertisingAPIBundle\API\Configuration',
+            $this->configuration->clear(),
+    		"A self object wasn't returned when invoking clear().");
+        return $this->configuration;
+    }
+
+    /**
+     * An empty array will be returned after invoking clear
+     *
+     * @depends testSelfObjectWillBeReturnedWhenInvokingClear
+     * @param Configuration $configuration
+     */
+    public function testAnEmptyArrayWillBeReturnedAfterInvokingClear(Configuration $configuration)
+    {
+        $this->assertEmpty($this->configuration->toArray(), "Empty array was returned.");
+    }
+
+    /**
+     * Has the parameter which is specified key
+     *
+     * @depends testSelfObjectWillBeReturnedWhenInvokingClear
+     * @param Configuration $configuration
+     */
+    public function testHasNotTheParameterWhichIsSpecifiedKey(Configuration $configuration)
+    {
+        $method = new \ReflectionMethod('Siny\Amazon\ProductAdvertisingAPIBundle\API\Configuration', 'has');
+        $method->setAccessible(true);
+        $this->assertFalse($method->invoke($configuration, self::DUMMY_KEY), "The parameter has.");
+    }
+}
